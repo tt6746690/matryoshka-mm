fix dist.all_gather does not backprop grad bug

llava/model/language_model/llava_llama.py
@@ -34,8 +34,28 @@ except:
     pass
 
 
+
+def lm_loss(logits, labels, vocab_size):
+    # typical LM loss
+    loss = None
+    if labels is not None:
+        # Shift so that tokens < n predict n
+        shift_logits = logits[..., :-1, :].contiguous()
+        shift_labels = labels[..., 1:].contiguous()
+        # Flatten the tokens
+        loss_fct = CrossEntropyLoss()
+        shift_logits = shift_logits.view(-1, vocab_size)
+        shift_labels = shift_labels.view(-1)
+        # Enable model parallelism
+        shift_labels = shift_labels.to(shift_logits.device)
+        loss = loss_fct(shift_logits, shift_labels)
+    return loss
+
+
 @dataclass
 class CausalLMOutputWithPastWithGatingProb(CausalLMOutputWithPast):
+    losses: Optional[torch.FloatTensor] = None
+    losses_lm: Optional[torch.FloatTensor] = None
     gating_prob: Optional[torch.FloatTensor] = None
 
 
@@ -151,20 +171,11 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         ####
 
 
+        loss_lm = lm_loss(logits, labels, self.config.vocab_size)
+
+
         if gating_prob_k is None:
-            # typical LM loss
-            loss = None
-            if labels is not None:
-                # Shift so that tokens < n predict n
-                shift_logits = logits[..., :-1, :].contiguous()
-                shift_labels = labels[..., 1:].contiguous()
-                # Flatten the tokens
-                loss_fct = CrossEntropyLoss()
-                shift_logits = shift_logits.view(-1, self.config.vocab_size)
-                shift_labels = shift_labels.view(-1)
-                # Enable model parallelism
-                shift_labels = shift_labels.to(shift_logits.device)
-                loss = loss_fct(shift_logits, shift_labels)
+            loss = loss_lm
         else:
             # LM loss weighted by gating prob
             loss = None
@@ -174,7 +185,6 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 shift_logits = logits[..., :-1, :].contiguous()
                 # (B, seq_len-1)
                 shift_labels = labels[..., 1:].contiguous()
-                valid_mask = (shift_labels != -100)
                 # Flatten the tokens
                 loss_fct_noreduce = CrossEntropyLoss(reduction='none')
                 # Enable model/pipeline parallelism
@@ -183,14 +193,16 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 losses = loss_fct_noreduce(shift_logits.view(-1, self.config.vocab_size), shift_labels.view(-1))
                 # (B, seq_len-1)
                 losses = losses.view(-1, logits.shape[1]-1)
-                # handle cases where some values in `valid_mask.sum(1)` is 0, causing division by 0
+                # average loss for each example in the batch
+                # while handle cases where some values in `valid_mask.sum(1)` is 0, causing division by 0
+                valid_mask = (shift_labels != -100)
                 loss = (losses * valid_mask).sum(1) / (valid_mask.sum(1) + 1e-8)
                 # (B,)
                 loss = loss * gating_prob_k.reshape_as(loss)
                 # (1,)
                 loss = loss.mean()
 
-        return loss, logits, outputs, gating_prob
+        return loss, logits, outputs, gating_prob, loss_lm
 
 
 
@@ -226,10 +238,11 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
             else:
                 raise ValueError(f"[llava.model.language_model.llava_llama.py] {kvs['ver']} not implemented.")
 
-            loss = 0
+            losses_accumulate = [] # can be weighted or not.
+            losses_lm_accumulate = [] # unweighted lm loss
             logits_accumulate = []
             for matryoshka_vis_token_scale_element in matryoshka_vis_token_scale:
-                loss_item, logits, outputs, gating_prob = self.forward_single_matryoshka(
+                loss_item, logits, outputs, gating_prob, loss_lm_item = self.forward_single_matryoshka(
                     input_ids = input_ids,
                     attention_mask = attention_mask,
                     position_ids = position_ids,
@@ -245,9 +258,10 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                     matryoshka_vis_token_scale = matryoshka_vis_token_scale_element,
                 )
                 if gating_prob is None:
-                    loss += loss_item/len(matryoshka_vis_token_scale)
-                else:
-                    loss += loss_item
+                    loss_item = loss_item/len(matryoshka_vis_token_scale)
+                    loss_lm_item = loss_lm_item/len(matryoshka_vis_token_scale)
+                losses_accumulate.append(loss_item)
+                losses_lm_accumulate.append(loss_lm_item)
                 logits_accumulate.append(logits)
                 # wpq: only logits & loss is the avg of the different scales.
                 #      `past_key_values`, `hidden_states`, `attentions` are from last scale only.
@@ -257,6 +271,9 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 # logits: (B, seq_len, vocab_size) ->  (B, seq_len_1+...+seq_len_K, vocab_size) where K=#token scales
                 assert len(outputs) == 1, 'len(outputs) == 1 is False'
             logits = torch.cat(logits_accumulate, dim = 1)
+            losses = torch.stack(losses_accumulate)
+            losses_lm = torch.stack(losses_lm_accumulate)
+            loss = losses.sum()
                     
             if not return_dict:
                 output = (logits,) + outputs[1:] + (gating_prob,)
@@ -267,6 +284,8 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 past_key_values=outputs.past_key_values,
                 hidden_states=outputs.hidden_states,
                 attentions=outputs.attentions,
+                losses=losses,
+                losses_lm=losses_lm,
                 gating_prob=gating_prob,
             )
             
@@ -318,6 +337,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 past_key_values=outputs.past_key_values,
                 hidden_states=outputs.hidden_states,
                 attentions=outputs.attentions,
+                losses=None,
                 gating_prob=gating_prob,
             )
 

llava/model/llava_arch.py
@@ -15,6 +15,7 @@
 
 from abc import ABC, abstractmethod
 
+import math
 import torch
 import torch.nn as nn
 
@@ -354,25 +355,26 @@ class LlavaMetaForCausalLM(ABC):
         H = W = int(self.get_model().get_vision_tower().config.image_size / self.get_model().get_vision_tower().config.patch_size)
         kvs = parse_kv_from_string(matryoshka_vis_token_scale)
 
-        if kvs['ver'] == 'v0':
+        if kvs['ver'] == 'v0': 
             if kvs['numtoks'] == 'gateprobargmax':
                 if gating_prob is None:
                     raise ValueError(f'[LlavaMetaForCausalLM.project_v4] requires `gating_prob` to select the right token scale for matryoshka_vis_token_scale={matryoshka_vis_token_scale}')
                 if image_features.shape[0] != 1:
                     raise ValueError(f'[LlavaMetaForCausalLM.project_v4] only support batch_size=1 for matryoshka_vis_token_scale={matryoshka_vis_token_scale} but got {image_features.shape[0]}. This is ok since only used during inference.')
                 numtoks_idx = torch.argmax(gating_prob.squeeze(0)).item()
-                token_scales = eval(parse_kv_from_string(self.config.config['matryoshka_vis_token_scale'])['numtoks'])
-                numtoks = token_scales[numtoks_idx]
+                numtoks = self.get_model().tokscale_list[numtoks_idx]
             else:
                 numtoks = int(kvs['numtoks'])
 
             B, H_W, D = image_features.shape
             reshaped_tensor = image_features.view(B, H, W, D)
-            # (B, D, H, W)
+            # (B, D, H, W) e.g., (B, 4096, 24, 24)
             reshaped_tensor = reshaped_tensor.permute(0, 3, 1, 2)
-            pool_size = stride = int( np.sqrt(H_W / numtoks) )
-            # (B, D, 3, 3) if numtoks=9
-            pooled_tensor = torch.nn.functional.avg_pool2d(reshaped_tensor, kernel_size=pool_size, stride=stride)
+            h = w = int( math.sqrt(numtoks) )
+            assert(h*w == numtoks)
+            # (B, D, h, w)
+            pooled_tensor = torch.nn.functional.adaptive_avg_pool2d(reshaped_tensor, (h, w))
+            # (B, h, w, D)
             image_features = pooled_tensor.permute(0, 2, 3, 1)
             # (B, numtoks, D)
             image_features = image_features.reshape(B, -1, D)

llava/train/llava_trainer.py
@@ -441,35 +441,57 @@ class LLaVATrainer(Trainer):
             loss_mb = smp_forward_backward(model, inputs, self.args.gradient_accumulation_steps)
             return loss_mb.reduce_mean().detach().to(self.args.device)
 
+        # wpq: to get `ModelOutputs` instead of tuple.
+        inputs.update({'return_dict': True})
+
         with self.compute_loss_context_manager():
             loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
+
+        ## wpq: log per-expert LM loss
+        if outputs.losses is not None: # m3 type training
+            if self.is_world_process_zero() and 'wandb' in self.args.report_to:
+                log_dict = {}
+                for k in range(outputs.losses.numel()):
+                    log_dict.update({f'moe/loss_{k}': outputs.losses[k].item()})
+                    log_dict.update({f'moe/loss_lm_{k}': outputs.losses_lm[k].item()})
+
 
         ### wpq: MoE load balancing loss.
         if self.model.get_model().is_m3_moe:
             # use `self.model` to access llama model.
             kvs = parse_kv_from_string(self.model.config.config.get('moe', None))
+            tokscales = eval(parse_kv_from_string(self.model.config.config.get('matryoshka_vis_token_scale', None)).get('numtoks', None))
+
+            # (micro-bsz, K)
+            gating_prob = outputs.gating_prob
+            device, dtype = gating_prob.device, gating_prob.dtype
+            assert(gating_prob.shape[1] == len(tokscales))
+
+            with torch.no_grad():
+                # gather `gating_prob`` (micro-bsz, K) -> (B, K) where K is number of experts.
+                gating_prob_list = [torch.zeros_like(outputs.gating_prob) for _ in range(self.args.world_size)]
+                dist.all_gather(gating_prob_list, outputs.gating_prob)
+                batch_gating_prob = torch.cat(gating_prob_list, dim=0)
+                B, K = batch_gating_prob.shape
+                # (K,)
+                batch_per_expert_gating_prob = torch.mean(gating_prob, dim=0)
+                # (K,)
+                batch_per_expert_assignment = torch.nn.functional.one_hot(batch_gating_prob.argmax(dim=1), num_classes=K)
+                batch_per_expert_assignment = batch_per_expert_assignment.sum(dim=0).float() / B
+                batch_per_expert_assignment = batch_per_expert_assignment.to(device).to(dtype)
+
+            if self.is_world_process_zero() and 'wandb' in self.args.report_to:
+                for k in range(K):
+                    log_dict.update({f'moe/avg_gating_prob_{k}': batch_per_expert_gating_prob[k].item()})
+                    log_dict.update({f'moe/avg_expert_assignment_{k}': batch_per_expert_assignment[k].item()})
+
             ## compute switch transformer load balance loss: https://dl.acm.org/doi/pdf/10.5555/3586589.3586709
             if kvs.get('loadb', None) == 'switch':
                 alpha = float(kvs['alpha'])
-                tokscales = eval(parse_kv_from_string(self.model.config.config.get('matryoshka_vis_token_scale', None)).get('numtoks', None))
-                gating_prob_idx = len(outputs)-1 # loss, logits, gating_prob
-                gating_prob = outputs[gating_prob_idx]
-                device, dtype = gating_prob.device, gating_prob.dtype
-                assert(outputs[gating_prob_idx].shape[1] == len(tokscales))
-
-                # gather `gating_prob`` (micro-bsz, K) -> (B, K) where K is number of experts.
-                gating_prob_list = [torch.zeros_like(outputs[gating_prob_idx]) for _ in range(self.args.world_size)]
-                dist.all_gather(gating_prob_list, outputs[gating_prob_idx])
-                gating_prob = torch.cat(gating_prob_list, dim=0)
-                B, K = gating_prob.shape
-
-                per_expert_assignment = torch.nn.functional.one_hot(gating_prob.argmax(dim=1), num_classes=K)
-                per_expert_assignment = per_expert_assignment.sum(dim=0).float() / B
-                per_expert_assignment = per_expert_assignment.to(device).to(dtype)
                 
                 per_expert_cost_type = kvs.get('costt', 'count')
                 if per_expert_cost_type == 'count': # default used in switch transformers
-                    per_expert_cost = per_expert_assignment
+                    per_expert_cost = batch_per_expert_assignment
                 elif per_expert_cost_type == 'numtoks':
                     per_expert_cost = torch.tensor(tokscales, device=device, dtype=dtype)
                     per_expert_cost = per_expert_cost / per_expert_cost.sum()
@@ -478,35 +500,33 @@ class LLaVATrainer(Trainer):
                     per_expert_cost = torch.log(per_expert_cost+1) # add 1 to prevent cost(tokscale=1)=0
                     per_expert_cost = per_expert_cost / per_expert_cost.sum()
                 elif per_expert_cost_type == 'count*numtoks':
-                    per_expert_cost = torch.nn.functional.one_hot(gating_prob.argmax(dim=1), num_classes=K)
-                    per_expert_cost = per_expert_cost.sum(dim=0).float() / B
+                    per_expert_cost = batch_per_expert_assignment
                     per_expert_cost_2 = torch.tensor(tokscales, device=device, dtype=dtype)
                     per_expert_cost_2 = per_expert_cost_2 / per_expert_cost_2.sum()
                     per_expert_cost *= per_expert_cost_2
+                    per_expert_cost = per_expert_cost / per_expert_cost.sum()
                 elif per_expert_cost_type == 'count*lognumtoks':
-                    per_expert_cost = torch.nn.functional.one_hot(gating_prob.argmax(dim=1), num_classes=K)
-                    per_expert_cost = per_expert_cost.sum(dim=0).float() / B
+                    per_expert_cost = batch_per_expert_assignment
                     per_expert_cost_2 = torch.tensor(tokscales, device=device, dtype=dtype)
                     per_expert_cost_2 = torch.log(per_expert_cost_2+1) # add 1 to prevent cost(tokscale=1)=0
                     per_expert_cost_2 = per_expert_cost_2 / per_expert_cost_2.sum()
                     per_expert_cost *= per_expert_cost_2
+                    per_expert_cost = per_expert_cost / per_expert_cost.sum()
                 else:
                     raise ValueError(f'per_expert_cost_type={per_expert_cost_type} not supported.')
                 
-                per_expert_gating_prob = torch.mean(gating_prob, dim=0)
                 # (K,), (K,) -> (,)
-                loss_switch = alpha * K * (per_expert_cost * per_expert_gating_prob).sum()
+                loss_switch = alpha * K * (per_expert_cost * torch.mean(gating_prob, dim=0)).sum()
                 loss += loss_switch
+                
+                if self.is_world_process_zero() and 'wandb' in self.args.report_to:
+                    log_dict.update({'moe_load/loss_switch': loss_switch.item(),})
+                    for k in range(K):
+                        log_dict.update({f'moe_load/cost_{k}': per_expert_cost[k].item()})
 
-                if self.is_world_process_zero():
-                    if 'wandb' in self.args.report_to: # log once/batch if assume no gradient accumulation.
-                        log_dict = {}
-                        log_dict.update({'moe_load/loss_switch': loss_switch.item(),})
-                        for k in range(K):
-                            log_dict.update({f'moe_load/avg_gating_prob_{k}': per_expert_gating_prob[k].item()})
-                            log_dict.update({f'moe_load/avg_expert_assignment_{k}': per_expert_assignment[k].item()})
-                            log_dict.update({f'moe_load/cost_{k}': per_expert_cost[k].item()})
-                        wandb.log(log_dict)
+         # log once/batch if assume no gradient accumulation.
+        if self.is_world_process_zero() and 'wandb' in self.args.report_to:
+            wandb.log(log_dict)
 
         if self.args.n_gpu > 1:
             loss = loss.mean()  # mean() to average on multi-gpu parallel training